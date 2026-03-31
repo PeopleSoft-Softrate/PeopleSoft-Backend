dc91180cf5324ccaaa8b3f755be904f161d71154
Yovel-R <yovel2911@gmail.com>
2026-03-31 11:37:13 +0530

    clust 5mb

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ require("./cron/leaveReset.cron");
 
 const app = express();
 app.use(cors({ origin: '*' }));
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ limit: '5mb', extended: true }));
 app.use(express.static('public'));
 
 // ============================
